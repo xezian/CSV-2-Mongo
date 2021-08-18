7b51062832710a0852a4aba019eb760f0fc63216
Jason Leo <30601823+xezian@users.noreply.github.com>
2021-08-18 17:51:33 -0400

    Refactor validation logic out of main handler

--- src/handler.py
+++ src/handler.py
@@ -56,22 +56,13 @@ def handle_csv_upload(event, context):
 
     # Loop through list of row dicts
     for entry in entries:
-        # Validate fields
-        valid_email = (
-            entry["Email"] if re.match(r"[^@]+@[^@]+\.[^@]+", entry["Email"]) else None
-        )
-        valid_name = (
-            entry["Name"]
-            if all(x.isalpha() or x.isspace() for x in entry["Name"])
-            else None
-        )
-        valid_salary = validate_salary(entry["Salary"])
-        valid_manager_email = (
-            entry["Manager"]
-            if re.match(r"[^@]+@[^@]+\.[^@]+", entry["Manager"])
-            else None
-        )
-        valid_hire_date = validate_hire_date(entry["Hire Date"])
+        (
+            valid_email,
+            valid_name,
+            valid_salary,
+            valid_manager_email,
+            valid_hire_date,
+        ) = validate_entry(entry)
 
         # Process validated fields into errors
         if not valid_email:
@@ -95,16 +86,16 @@ def handle_csv_upload(event, context):
             )
 
         update_user = {}
-        valid_manager = None
+        manager = None
         # Update name, salary, manager_id, hire_date
         if valid_name:
             update_user["name"] = valid_name
         if valid_salary:
             update_user["salary"] = valid_salary
         if valid_manager_email:
-            valid_manager = db.user.find_one({"normalized_email": valid_manager_email})
-            if valid_manager:
-                update_user["manager_id"] = valid_manager["_id"]
+            manager = db.user.find_one({"normalized_email": valid_manager_email})
+            if manager:
+                update_user["manager_id"] = manager["_id"]
         if valid_hire_date:
             update_user["hire_date"] = valid_hire_date
 
@@ -116,22 +107,37 @@ def handle_csv_upload(event, context):
         else:
             response_body["numCreated"] += 1
 
-        if valid_manager:
-            user = db.user.find_one({"normalized_email": valid_email})
-            manager_chain = db.chain_of_command.find_one(
-                {"user_id": valid_manager["_id"]}
-            )
+        user = db.user.find_one({"normalized_email": valid_email})
+        managers = []
+        if manager:
+            manager_chain = db.chain_of_command.find_one({"user_id": manager["_id"]})
             managers = manager_chain["chain_of_command"]
-            managers.append(valid_manager["_id"])
-            db.chain_of_command.update(
-                {"user_id": user["_id"]},
-                {"$set": {"chain_of_command": managers}},
-                upsert=True,
-            )
-
+            managers.append(manager["_id"])
+        db.chain_of_command.update(
+            {"user_id": user["_id"]},
+            {"$set": {"chain_of_command": managers}},
+            upsert=True,
+        )
     response = {"statusCode": 200, "body": json.dumps(response_body)}
-    print(response)
     return response
+
+
+def validate_entry(entry):
+    # Validate fields
+    valid_email = (
+        entry["Email"] if re.match(r"[^@]+@[^@]+\.[^@]+", entry["Email"]) else None
+    )
+    valid_name = (
+        entry["Name"]
+        if all(x.isalpha() or x.isspace() for x in entry["Name"])
+        else None
+    )
+    valid_salary = validate_salary(entry["Salary"])
+    valid_manager_email = (
+        entry["Manager"] if re.match(r"[^@]+@[^@]+\.[^@]+", entry["Manager"]) else None
+    )
+    valid_hire_date = validate_hire_date(entry["Hire Date"])
+    return valid_email, valid_name, valid_salary, valid_manager_email, valid_hire_date
 
 
 def validate_salary(salary):
